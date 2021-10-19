3a431fc99e3bd5fce45fe60ffca64a544743a4b9
Sanu Singh Rajput <81958742+Sanu-Singh@users.noreply.github.com>
2021-10-20 04:00:15 +0530

    Add files via upload

---
+++ Traversal.cpp
@@ -0,0 +1,28 @@
+// operations on Arrays in DSA
+// 1) Traversal:Visiting or printing every element of array
+// a[5] ={2,5,8,0}
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int size,i;
+	int arr [50];
+	
+	cout<<"Enter the size of arrays: "<<endl;
+	cin>>size;
+	
+	cout<<"Enter the elements of arrays:"<<" ";
+	for(i=0; i<size; i++)
+	{
+		cin>>arr[i];	
+	}
+	
+	cout<<"Elemente in array are:"<<endl;
+	for(i=0; i<size; i++)
+	{
+		cout<<arr[i]<<' ';n
+	 } 
+	return 0;
+	
+}
